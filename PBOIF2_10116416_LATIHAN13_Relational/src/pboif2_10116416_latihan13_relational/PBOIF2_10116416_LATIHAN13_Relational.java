6efd67fa05f856e8090554fa46fdf083ccb6d0a9
baharriyan <indonesiariyan@gmail.com>
2020-10-20 19:04:59 +0700

    first commit

---
+++ PBOIF2_10116416_LATIHAN13_Relational/src/pboif2_10116416_latihan13_relational/PBOIF2_10116416_LATIHAN13_Relational.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116416_latihan13_relational;
+
+/**
+ *
+ * @author Riyan Bahar
+ * Nama  : Riyan Bahar
+ * Kelas : IF-2
+ * Nim   : 10116416
+ * 
+ */
+public class PBOIF2_10116416_LATIHAN13_Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b =  " + (bil1 == bil2));
+        System.out.println("a != b =  " + (bil1 != bil2));
+        System.out.println("a > b =  " + (bil1 > bil2));
+        System.out.println("a < b =  " + (bil1 < bil2));
+        System.out.println("b >= a =  " + (bil1 >= bil2));
+        System.out.println("b <= a =  " + (bil1 <= bil2));
+    }
+    
+}
